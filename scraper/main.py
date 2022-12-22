b054679c6ae8be047e072bb8ff989df1146f231f
Jakub Kucera <jakub.kucerja@gmail.com>
2022-12-22 01:33:59 +0100

    Improve Scraper task

--- scraper/main.py
+++ scraper/main.py
@@ -1,3 +1,4 @@
+import argparse
 import threading
 from dataclasses import dataclass
 from datetime import datetime
@@ -15,12 +16,18 @@ REQUESTS_HEADERS = {
 
 FORBIDDEN_FILE_TYPES = ['pdf', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'zip', 'rar', '7z', 'tar', 'gz', 'bz2', 'mp3', 'mp4', 'avi', 'mkv', 'flv', 'wmv', 'mov', 'jpg', 'jpeg', 'png', 'gif', 'bmp', 'svg', 'ico', 'psd', 'ai', 'eps', 'tif', 'tiff', 'wav', 'mp3', 'ogg', 'flac', 'aac', 'wma', 'm4a', 'exe', 'dll', 'deb', 'rpm', 'apk', 'iso', 'dmg', 'odt', 'txt', 'csv', 'tsv']
 
-START_URL = "https://fit.cvut.cz/"
-END_URL = "https://www.mit.edu/"
-END_URL_CORE = '.'.join(urlparse(END_URL).netloc.split('.')[-2:])
+DEFAULT_START_URL = "https://fit.cvut.cz/"
+DEFAULT_END_URL = "https://www.mit.edu/"
+# DEFAULT_END_URL_CORE = '.'.join(urlparse(DEFAULT_END_URL).netloc.split('.')[-2:])
+
+
+# global variables for collections of opened and closed nodes
+opened: set['Node'] = set()
+closed: set['Node'] = set()
 
 
 def score_url(url: str, prev_url: str):
+    """Scores a url in order to help the search. In terms of A* this would be the heurestic function"""
     score = 0
     # prioritise pages with an 'edu' domain
     if urlparse(url).netloc.split('.')[-1] != 'edu':
@@ -42,6 +49,7 @@ def score_url(url: str, prev_url: str):
 
 
 def skip_url(url: str):
+    """Determines whether an url should be skipped or not"""
     if not validators.url(url):
         return True
 
@@ -53,6 +61,9 @@ def skip_url(url: str):
 
 
 class Node:
+    """
+    A Node which represents 1 url and some metadata about it.
+    """
     def __init__(self, url: str, parent: 'Node'):
         self.url: str = url
         self.parent: Node = parent
@@ -73,14 +84,16 @@ class Node:
         return NotImplemented
 
 
-opened: set[Node] = set()
-closed: set[Node] = set()
-
-
-def worker():
+def run(end_url_core: str):
     while len(opened) > 0:
+        # get the node with the lowest score
         current_node = min(opened, key=lambda x: x.total_score)
+
+        # push to closed nodes.
+        # (This was moved here from the end of the loop, to improve behaviour during
+        # parallel computing, which was eventually deleted since it was not needed.)
         closed.add(current_node)
+        # remove from opened nodes
         opened.remove(current_node)
         url = current_node.url
 
@@ -90,7 +103,7 @@ def worker():
         print(f"{current_node.steps} + {current_node.url_score} = {current_node.total_score}")
         print(f"opened: {len(opened)} | closed: {len(closed)}")
 
-        request_start = datetime.now()
+        # get the content of the page
         try:
             r = requests.get(url, headers=REQUESTS_HEADERS)
         except requests.exceptions.SSLError:
@@ -102,13 +115,14 @@ def worker():
         except requests.exceptions.ConnectionError:
             print(f"<====== Connection error for {url}")
             continue
-        request_end = datetime.now()
-        print(f"request time: {request_end - request_start}")
 
+        # parse the content
         soup = BeautifulSoup(r.text, 'html.parser')
+        # find all links on the page
         new_urls = soup.findAll('a')
         for new_urls_raw in new_urls:
             new_url = new_urls_raw.get('href')
+
             # if new_url points to the same page, it is of a relative path, so it needs to be joined.
             # (if url and new_url are of a different domain, urljoin will return new_url)
             joined_new_url = urljoin(url, new_url)
@@ -117,12 +131,13 @@ def worker():
                 continue
 
             discovered_node = Node(joined_new_url, current_node)
+            # check if this url node has been already discovered before
             if discovered_node not in opened and discovered_node not in closed:
                 opened.add(discovered_node)
 
-                if END_URL_CORE in joined_new_url:
+                # check if the url is the END_URL
+                if end_url_core in joined_new_url:
                     print(f"FOUND: {joined_new_url}")
-                    # opened.clear()
                     return discovered_node
 
     return None
@@ -134,19 +149,35 @@ def print_path(node: Node):
     print(node.url, end=' -> ')
 
 
-def run():
+def main(start_url: str, end_url: str):
     start = datetime.now()
-    start_node = Node(START_URL, None)
+
+    # # add the HTTPS prefix if it is missing
+    # start_url = urlparse(start_url, 'https').geturl()
+    # end_url = urlparse(end_url, 'https').geturl()
+
+    start_node = Node(start_url, None)
     opened.add(start_node)
 
+    end_url_core = '.'.join(urlparse(end_url).netloc.split('.')[-2:])
     # the searching part
-    dest_node = worker()
+    dest_node = run(end_url_core)
 
     print(60*'=')
     print_path(dest_node)
+
 
     print(f"\nTime taken: {datetime.now() - start}")
 
 
 if __name__ == '__main__':
-    run()
+    parser = argparse.ArgumentParser(prog='scraper.py', description="This program scrawls the web and find a path from a given start url to an end url.")
+    parser.add_argument('-s', '--start', action='store', default=DEFAULT_START_URL,
+                        metavar='START', type=str,
+                        help='The start url. Include the HTTPS/HTTP prefix. Default: ' + DEFAULT_START_URL)
+    parser.add_argument('-e', '--end', action='store', default=DEFAULT_END_URL,
+                        metavar='END', type=str,
+                        help='The end url. Include the HTTPS/HTTP prefix. Default: ' + DEFAULT_END_URL)
+
+    args = parser.parse_args()
+    main(start_url=args.start, end_url=args.end)
