8945bfa3367f64411f03a90578affe2b6390faae
Jakub Kucera <jakub.kucerja@gmail.com>
2022-12-23 15:38:17 +0100

    Move graph logic into a separate Class

--- scp/main.py
+++ scp/main.py
@@ -28,7 +28,7 @@ class Node:
         self.neighbors = []
         self.available_colors = ALL_COLORS_LIST.copy()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"<{self.name} | {self.color}>"
 
     def add_neighbor(self, node):
@@ -37,7 +37,7 @@ class Node:
     def remove_potential_color(self, color: Color):
         self.available_colors.remove(color)
 
-    def check_constraints(self):
+    def check_constraints(self) -> bool:
         # ignore for uncolored node
         if self.color is None:
             return True
@@ -46,38 +46,66 @@ class Node:
                 return False
         return True
 
-    def uncolored_neighbors(self):
+    def uncolored_neighbors(self) -> list['Node']:
         return [neighbor for neighbor in self.neighbors if neighbor.color is None]
 
-    def uncolored_neighbors_count(self):
+    def uncolored_neighbors_count(self) -> int:
         return len(self.uncolored_neighbors())
 
+    def is_assigned(self) -> bool:
+        return bool(self.color)
 
-def create_node_graph(str_graph: dict[str, list[str]]) -> list[Node]:
-    country_nodes = []
-    country_str_to_node = {}
 
-    # create nodes for countries
-    for country in SCP_MAP.keys():
-        country_node = Node(country)
-        country_str_to_node[country] = country_node
-        country_nodes.append(country_node)
+class Graph:
+    def __init__(self, str_graph: dict[str, list[str]]):
+        self.nodes = []
+        country_str_to_node = {}
 
-    # add neighbors to nodes
-    for country, neighbors in SCP_MAP.items():
-        country_node = country_str_to_node[country]
-        for neighbor in neighbors:
-            country_node.add_neighbor(country_str_to_node[neighbor])
+        # create nodes for countries
+        for country in SCP_MAP.keys():
+            country_node = Node(country)
+            country_str_to_node[country] = country_node
+            self.nodes.append(country_node)
 
-    # print nodes and their neighbors. for DEBUG
-    for country_node in country_nodes:
-        print(f"{country_node}: {country_node.neighbors}")
+        # add neighbors to nodes
+        for country, neighbors in SCP_MAP.items():
+            country_node = country_str_to_node[country]
+            for neighbor in neighbors:
+                country_node.add_neighbor(country_str_to_node[neighbor])
 
-    return country_nodes
+        # print nodes and their neighbors. for DEBUG
+        for country_node in self.nodes:
+            print(f"{country_node}: {country_node.neighbors}")
+
+    def check_nodes_constraints(self) -> bool:
+        for node in self.nodes:
+            if not node.check_constraints():
+                return False
+        return True
+
+    def is_complete(self) -> bool:
+        # ? maybe check if constraints are satisfied ?
+        for node in self.nodes:
+            if not node.is_assigned():
+                return False
+        return True
+
+    def get_unassigned_node(self) -> Node | None:
+        # TODO maybe pick based on MAX node.uncolored_neighbors_count
+        for node in self.nodes:
+            if not node.is_assigned():
+                return node
+        return None
+
+
+def backtrack():
+    # if complete, return
+    pass
 
 
 def main():
-    country_nodes = create_node_graph(SCP_MAP)
+    # country_nodes = create_node_graph(SCP_MAP)
+    graph = Graph(SCP_MAP)
 
 
 # Press the green button in the gutter to run the script.
